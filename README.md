c76f534550fbce1a239b67aeb704945e31f55df0
MrIncognicto <63042426+MrIncognicto@users.noreply.github.com>
2020-05-27 23:17:24 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 my personal Voice Assistantwritten in Python.
 Your Welcome to Contibute and add some cool features
 
-Got an Inspirationn of making Something like this From a Movie IronMan and the bussy Tony Stark
-
 The Name E.D.I.T.H stand for Even Dead Im The Hero
+
+Required Packages
+speechRecognition
+Pyttsx3
+PlaySound
+
 
 Made in Python
--- main.py
+++ main.py
@@ -1,47 +1,110 @@
+import speech_recognition as sr 
 import pyttsx3
 import datetime
 import time
 import webbrowser
-import json
-import requests
+from playsound import playsound
 
 now= datetime.datetime.now()
 current_time =now.strftime("%H:%M")
+current_time = str(current_time)
 Today=datetime.date.today()
-advice = requests.get("https://api.adviceslip.com/advice")
-advice = advice.json()
 
-#speak
-engine =pyttsx3.init()
-engine.setProperty('rate', 133)
-voices = engine.getProperty('voices')
-for voice in voices:
-    engine.setProperty('voice', 'english-us+f2')
+class person:
+	name = ''
+	def setName(self,name):
+		self.name=name
 
-def talk():
-	ask =input('Hey There!, How can I help you?: ' ).lower()
-	if "current time" in ask:
-		engine.say("The current time is:", current_time)
-		print(current_time)
+def there_exist(terms):
+	for term in terms:
+		if term in text:
+			return True
 
-	if "your name" in ask:
-		engine.say('My name is EDITH')
-		print('EDITH')
 
-	if "what does edith mean?" in ask:
-		print('EDITH stands for even dead im the hero')
+def speak(tts):
+	engine =pyttsx3.init()
+	engine.setProperty('rate', 150)
+	engine.say(tts)
+	engine.runAndWait()
 
-	if "Which day is today?" in ask:
-		print('Today is', Today)
+r =sr.Recognizer()
+def listen():
+	with sr.Microphone() as source:
 
-	if ask in ['tell an advice' 'advice me' 'give an advice ']:
-		print(advice['slip']['advice'])
+		audio= r.listen(source)
+		text = ''
 
-	if "search google" in ask:
-		url='https://www.google.com'
+		try:
+			text = r.recognize_google(audio)
+		except sr.UnknownValueError:
+			speak('Sorry, I did not get that')
+		except sr.RequestError:
+			speak('Sorry, It seems you have connection issues. Check your internet connection')
+		return text.lower()
+
+speak('Hello there, How can I help you?')
+print('How can I help You')
+
+text = listen()
+
+def response(audio_file):
+	#Greeting
+	if there_exist(['Hi', 'Hello', 'Hey Edith', 'Edith']) :
+		speak("Hey there")
+
+
+	#save user name
+	if there_exist(["my name is"]):
+		person_name = text.split("is")[-1].strip()
+		speak(f"Ok i will rember that {person_name}")
+		character.setName(person_name)
+	#Time 
+	if there_exist(['current time', 'what is the time', 'what is the current time']):
+		speak('The current time is '+ current_time)
+
+	#name
+	
+	try:
+		if there_exist(['my name', 'what is my name', 'who am i']):
+			speak(f'Your name is {person_name}')
+	except UnboundLocalError:
+		speak('I dont know yet who you are, who do you want me to call you?')
+		person_name = text.split('me') [-1].strip()
+		character.setName(person_name)
+		if there_exist(['call me']):
+			speak(f'okey i will remember that {person_name}')
+
+
+	if there_exist(['your name', 'who are you', 'how can i call you?', 'what is your name']):
+		speak('Im EDITH, Your personal assistant')
+
+
+	#Day
+	if there_exist(['which day is today', 'when is today', 'today']):
+		speak('today is' +Today)
+	
+	#search google
+	if there_exist (['search for']) and 'youtube' not in text:
+		search_term = text.split('for')[-1]
+		url = f"https://www.google.com/search?q={search_term}"
 		webbrowser.get().open(url)
+		speak(f"Here is what i found on {search_term} ")
+
+	if there_exist(['youtube']):
+		search_term =text.split('for')[-1]
+		url =f"https://www.youtube.com/results?search_query={search_term}"
+		webbrowser.get().open(url)
+		speak(f"Here is what i found on {search_term} in youtube")
 
 
+	if there_exist(['quit', 'exit', 'leave', 'bye' ,'good bye']):
+		speak('Bye, See you when you need my help')
+		exit()
 
-talk()
-engine.runAndWait()
+time.sleep(1)
+
+character =person()
+
+while (1):
+	text = listen()
+	response(text)
